2b95d7012390fd64e0f1c9e962fa553247fe71c0
Gr-Stanislav <grstas1990@gmail.com>
2022-08-13 17:54:18 +0300

    add new color

--- index.js
+++ index.js
@@ -6,14 +6,17 @@
 // form.addEventListener('submit', handleForm);
 
 const modalWindow = document.querySelector('.post_form'),
+      form = document.querySelector('.form'),
       btnSubmit = document.querySelector('.submit');
 
 // Модальное окно отправки формы------------------   
-btnSubmit.addEventListener('click', () => {
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
     modalWindow.classList.remove('hidden');
 
     setTimeout(() => {
         modalWindow.classList.add('hidden');
+        form.reset();
     }, 2000);
 });
 
